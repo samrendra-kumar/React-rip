0025ac5677154b3fd59dcb808b8b7064610fe06c
Samrendra <samrendrakumar893@gmail.com>
2024-06-04 18:26:02 +0530

    Syntactical sugar of jsx

--- App.js
+++ App.js
@@ -2,32 +2,35 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//React element
+const Heading=()=>(
+    <h1 id="title" key="h2">
+        Namaste React
+    </h1>
+)
 
-const heading=React.createElement("h1",
+ 
+    
+//React  component
+const HeaderComponent = () => 
     {
-        id:"title",
-    },
-    "Namaste Everyone!");
-
-    const heading1=React.createElement("h1",
-    {
-      id:"title",
-    },
-    "Heading 1");
-
-    const heading2=React.createElement("h2",
-    {
-     id:"title",   
-    },"Heading 2");
-
-    const container=React.createElement(
-        "div",
-        {
-            id:"container",
-        },
-        [heading1,heading2]
-    )
+        return (
+            <div>
+             <Heading/>   {/*component composition (component inside the component ) */}
+                  <h1>functional component </h1>
+                  <h1>functional component </h1>
+            </div>
+          
+        )
+    }
+//without return statement even we can write that 
+const HeaderComponent2 = ()=>(
+    <div>
+        <h1>Namaste react functional component</h1>
+        <h2>This is a h2 tag</h2>
+    </div>
+)
  
 
     const root=ReactDOM.createRoot(document.getElementById("root"));
-    root.render(container);
+    root.render(<HeaderComponent/>);
